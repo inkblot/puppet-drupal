77c357ac7fffad7565206ab33d96c46cb36ea5c8
Nate Riffe <inkblot@movealong.org>
2012-11-09 07:08:47 -0600

    Take off the curlies from a bare variable reference

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -16,7 +16,7 @@ define drupal::site(
 
 	exec { "drush-si-${name}":
 		command => "drush si standard -r ${drupal::params::drupal_path} -l ${name} --site-name=\"${sitename}\" --db-url=mysql://${db_user}:${db_password}@${db_server}/${db_name} --db-su=root --db-su-pw=${drupal::db_root_password} --account-username=${admin_user} --account-password=${admin_password}",
-		creates => ${site_dir},
+		creates => $site_dir,
 		path    => [ '/bin', '/usr/bin', '/sbin', '/usr/sbin' ],
 		require => Package[$::drupal::params::drush_package],
 	}
